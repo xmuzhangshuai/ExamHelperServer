931f8ee9aeaa86fb71fca4cdd086a9d2e0d10645
xmuzhangshuai <1207509737@qq.com>
2014-04-17 17:22:37 +0800

    删除考试指南类

--- ExamHelper/src/com/yrw/servlet/ExamGuideServlet.java
+++ ExamHelper/src/com/yrw/servlet/ExamGuideServlet.java
@@ -2,16 +2,15 @@ package com.yrw.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.yrw.tools.FastJsonTools;
-
 public class ExamGuideServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 109174335439235076L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,12 +19,7 @@ public class ExamGuideServlet extends HttpServlet {
 		response.setCharacterEncoding("utf-8");
 		request.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
-
-		ExamGuideService examGuideService = new ExamGuideService();
 		String msg = "";
-
-		List<ExamGuideType> examGuideTypes = examGuideService.getExamGuideTypeList();
-		msg = FastJsonTools.createJsonString(examGuideTypes);
 
 		out.write(msg);
 		out.flush();
@@ -40,15 +34,7 @@ public class ExamGuideServlet extends HttpServlet {
 		response.setCharacterEncoding("utf-8");
 		request.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
-
-		Long typeId = Long.parseLong(request.getParameter("ExamGuideTypeId"));
-
-		ExamGuideService examGuideService = new ExamGuideService();
-
 		String msg = "";
-
-		List<ExamGuide> examGuides = examGuideService.getExamGuideList(typeId);
-		msg = FastJsonTools.createJsonString(examGuides);
 
 		out.write(msg);
 		out.flush();
