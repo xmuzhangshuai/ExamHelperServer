4f13fa79ab380d073fa937f8ef32de69d0f59aa6
yeerin <837017745@qq.com>
2014-04-17 19:55:36 +0800

    新增examGuideDao
    Signed-off-by: yeerin <837017745@qq.com>

---
+++ ExamHelper/src/com/yrw/dao/ExamGuideDao.java
@@ -0,0 +1,73 @@
+package com.yrw.dao;
+
+import java.util.List;
+
+import com.yrw.domains.Examguide;
+import com.yrw.domains.Examguidetype;
+import com.yrw.idao.iExamGuideDao;
+
+public class ExamGuideDao extends BasicDao implements iExamGuideDao{
+
+	@Override
+	public List<Examguide> getExamguidesByExamguideTypeId(int examGuideTypeId ) {
+		// TODO Auto-generated method stub
+		String hql="from Examguide as e where e.examguidetype.id="+examGuideTypeId+" order by e.time desc";
+		return this.executeQuery(hql, null);
+	}
+
+	@Override
+	public List<Examguidetype> getExamguidetypesBySubjectId(int subjectId) {
+		// TODO Auto-generated method stub
+		String hql="from Examguidetype as e where e.subject.id="+subjectId;
+		return this.executeQuery(hql, null);
+	}
+
+	@Override
+	public Examguide getExamguideById(int examGuideId) {
+		// TODO Auto-generated method stub
+	return	(Examguide) this.findById(Examguide.class, examGuideId);
+	}
+
+	@Override
+	public Examguidetype getExamguidetypeById(int examGuideTypeId) {
+		// TODO Auto-generated method stub
+		return (Examguidetype) this.findById(Examguidetype.class, examGuideTypeId);
+	}
+
+	@Override
+	public void addExamguide(Examguide examguide) {
+		// TODO Auto-generated method stub
+		this.add(examguide);
+	}
+
+	@Override
+	public void addExamguidetype(Examguidetype examguidetype) {
+		// TODO Auto-generated method stub
+		this.addExamguidetype(examguidetype);
+	}
+
+	@Override
+	public void editExamguide(Examguide examguide) {
+		// TODO Auto-generated method stub
+		this.update(examguide);
+	}
+
+	@Override
+	public void editExamguideType(Examguidetype examguidetype) {
+		// TODO Auto-generated method stub
+		this.update(examguidetype);
+	}
+
+	@Override
+	public void deletExamguide(int  examguideId) {
+		// TODO Auto-generated method stub
+		this.deletById(Examguide.class, examguideId);
+	}
+
+	@Override
+	public void deletExamguideType(int examguidetypeId) {
+		// TODO Auto-generated method stub
+		this.deletById(Examguidetype.class, examguidetypeId);
+	}
+
+}
---
+++ ExamHelper/src/com/yrw/idao/iExamGuideDao.java
@@ -0,0 +1,59 @@
+package com.yrw.idao;
+
+import java.util.List;
+
+import com.yrw.domains.Examguide;
+import com.yrw.domains.Examguidetype;
+
+public interface iExamGuideDao extends IBasicDao {
+
+	/**获得考试指南
+	 * @return
+	 */
+	public List<Examguide> getExamguidesByExamguideTypeId(int examGuideTypeId);
+	/**获得考试指南类别
+	 * @return
+	 */
+	public List<Examguidetype> getExamguidetypesBySubjectId(int subjectId);
+	
+	/**通过考试指南id获得考试指南对象
+	 * @param examGuideId
+	 * @return
+	 */
+	public Examguide getExamguideById(int examGuideId);
+	/**通过id号获得考试指南类别
+	 * @param examGuideTypeId
+	 * @return
+	 */
+	public Examguidetype getExamguidetypeById(int examGuideTypeId);
+	
+	
+	/**增加考试指南
+	 * @param examguide
+	 */
+	public void addExamguide(Examguide examguide);
+	/**增加考试指南类别
+	 * @param examguidetype
+	 */
+	public void addExamguidetype(Examguidetype examguidetype);
+	
+	/**更新考试指南
+	 * @param examguide
+	 */
+	public void editExamguide(Examguide examguide);
+	/**跟新考试指南类别
+	 * @param examguide
+	 */
+	public void editExamguideType(Examguidetype examguidetype);
+	
+	/**删除examGuide对象
+	 * @param examguideId
+	 */
+	public void deletExamguide(int  examguideId);
+	/**删除考试指南类别
+	 * @param examguidetypeId
+	 */
+	public void deletExamguideType(int examguidetypeId);
+	
+	
+}
