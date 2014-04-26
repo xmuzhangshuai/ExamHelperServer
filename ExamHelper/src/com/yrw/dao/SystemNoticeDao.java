3db8ba79528382ad9145957764dee121a5a46beb
yeerin <837017745@qq.com>
2014-04-26 21:06:13 +0800

    Signed-off-by: yeerin <837017745@qq.com>

--- ExamHelper/src/com/yrw/dao/SystemNoticeDao.java
+++ ExamHelper/src/com/yrw/dao/SystemNoticeDao.java
@@ -1,5 +1,8 @@
 package com.yrw.dao;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import com.yrw.domains.Systemnotice;
 import com.yrw.idao.ISystemNoticeDao;
 
@@ -8,7 +11,50 @@ public class SystemNoticeDao extends BasicDao implements ISystemNoticeDao {
 	@Override
 	public Systemnotice getSystemnotice() {
 		// TODO Auto-generated method stub
-		return null;
+		String hql="from Systemnotice as s where s.valid=true";
+		return (Systemnotice) this.uniqueQuery(hql, null);
+	}
+
+	@Override
+	public void addSystmnotice(Systemnotice systemnotice) {
+		// TODO Auto-generated method stub
+		Systemnotice existSystemnotice=getSystemnotice();
+		existSystemnotice.setValid(false);
+		this.update(existSystemnotice);
+		
+		this.add(systemnotice);
+	}
+
+	@Override
+	public void deleteSystemnotice(Systemnotice systemnotice) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public List<Systemnotice> getSystemnoticeByPage(int pageNow) {
+		// TODO Auto-generated method stub
+		String hql="from Systemnotice as s order by s.time desc";
+		return this.executeQueryByPage(hql, null, pageNow);
+	}
+
+	@Override
+	public int getSystemnotciePageCount() {
+		// TODO Auto-generated method stub
+		String hql="select count(s) from Systemnotice as s";
+		return this.queryPageCount(hql, null);
+	}
+
+	@Override
+	public Systemnotice getSystemnoticeByMaxTime() {
+		// TODO Auto-generated method stub
+		String hql="select max(m.time) from Systemnotice as m";
+		Timestamp mxTime=(Timestamp) this.uniqueQuery(hql, null);
+		String timeString=mxTime.toString().split(".")[0];
+		System.out.println(timeString);
+		 hql="from Systemnotice as s where s.time="+timeString;
+		return (Systemnotice) this.uniqueQuery(hql, null);
 	}
 
 }
--- ExamHelper/src/com/yrw/idao/ISystemNoticeDao.java
+++ ExamHelper/src/com/yrw/idao/ISystemNoticeDao.java
@@ -1,7 +1,14 @@
 package com.yrw.idao;
 
+import java.util.List;
+
 import com.yrw.domains.Systemnotice;
 
 public interface ISystemNoticeDao extends IBasicDao {
 	public Systemnotice getSystemnotice();
+	public void addSystmnotice(Systemnotice systemnotice);
+	public void deleteSystemnotice(Systemnotice systemnotice);
+	public List<Systemnotice> getSystemnoticeByPage(int pageNow);
+	public int getSystemnotciePageCount();
+	public Systemnotice getSystemnoticeByMaxTime();
 }
