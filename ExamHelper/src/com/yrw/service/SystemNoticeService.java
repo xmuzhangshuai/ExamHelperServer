63963a3e2bc72f5ce8919a7d6c1ca7417c8fc45e
xmuzhangshuai <1207509737@qq.com>
2014-04-27 14:59:56 +0800

    新建发布答疑广场广告页面 Signed-off-by: xmuzhangshuai <1207509737@qq.com>

--- ExamHelper/src/com/yrw/service/SystemNoticeService.java
+++ ExamHelper/src/com/yrw/service/SystemNoticeService.java
@@ -1,7 +1,5 @@
 package com.yrw.service;
 
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 import com.yrw.domains.Systemnotice;
--- ExamHelper/src/com/yrw/servlet/SystemNoticeServlet.java
+++ ExamHelper/src/com/yrw/servlet/SystemNoticeServlet.java
@@ -11,38 +11,34 @@ import com.yrw.domains.Systemnotice;
 import com.yrw.service.SystemNoticeService;
 import com.yrw.tools.FastJsonTools;
 
+/**
+ * 类名称：SystemNoticeServlet
+ * 类描述：系统公告
+ * 创建人： 张帅
+ * 创建时间：2014年4月27日 上午9:18:59
+ */
 public class SystemNoticeServlet extends BaseServlet {
 
-//	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.setContentType("text/html;charset = utf-8");
-//		response.setCharacterEncoding("utf-8");
-//		request.setCharacterEncoding("utf-8");
-//		PrintWriter out = response.getWriter();
-//		String msg = "";
-//
-//		// 获取系统的业务逻辑组件
-//		SystemNoticeService noticeService = (SystemNoticeService) getApplicationContext().getBean("systemNoticeService");
-//
-//		Systemnotice systemnotice = noticeService.getCurrentNotice();
-//
-//		if (systemnotice != null) {
-//			msg = FastJsonTools.createJsonString(systemnotice);
-//		}
-//
-//		out.write(msg);
-//		out.flush();
-//		out.close();
-//	}
-//
-//	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset = utf-8");
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		String msg = "";
 
-	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
-	
+		// 获取系统的业务逻辑组件
+		SystemNoticeService noticeService = (SystemNoticeService) getApplicationContext()
+				.getBean("systemNoticeService");
+
+		Systemnotice systemnotice = noticeService.getCurrentNotice();
+
+		if (systemnotice != null) {
+			msg = FastJsonTools.createJsonString(systemnotice);
+		}
+
+		out.write(msg);
+		out.flush();
+		out.close();
 	}
+
 }
